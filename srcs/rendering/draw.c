0c82209a1f298522711a7b022a1ba87359ece8e7
yusengok <yusengok@student.42.fr>
2024-05-28 15:09:52 +0200

    Trying raycasting with camera plane

--- srcs/rendering/draw.c
+++ srcs/rendering/draw.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 08:57:21 by yusengok          #+#    #+#             */
-/*   Updated: 2024/05/28 08:57:54 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/05/28 13:25:10 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	draw_ceiling(t_cub3d *data, int x, int end, int ceiling_color)
 {
-	while (end >= 0)
+	while (end > 0)
 		put_pxl_color(&data->img, x, end--, ceiling_color);
 }
 
 void	draw_floor(t_cub3d *data, int x, int start, int floor_color)
 {
-	while (start <= WIN_H)
+	while (start < WIN_H)
 		put_pxl_color(&data->img, x, start++, floor_color);
 }
