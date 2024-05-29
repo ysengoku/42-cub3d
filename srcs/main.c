0894c84f3b0717bc48e8d2f791983f52cb82127e
yusengok <ysengoku.tourisme@gmail.com>
2024-05-29 16:50:04 +0200

    Something goes wrong in raycasting calculations. Neet to find it

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 08:09:57 by yusengok          #+#    #+#             */
-/*   Updated: 2024/05/29 13:12:21 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/05/29 16:31:45 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,16 @@
 
 /*== TEST CODE ===============================================================*/
 
-// 111111
-// 100101
-// 101001
-// 1100N1
-// 111111
-
-// int	test[5][6] =
-// {
-// 	{1,1,1,1,1,1},
-// 	{1,0,0,1,0,1},
-// 	{1,0,1,0,0,1},
-// 	{1,1,0,0,0,1},
-// 	{1,1,1,1,1,1}
-// };
+// 111111111111
+// 100001111101
+// 100000011001
+// 111100000001
+// 111100000001
+// 111100000001
+// 111100000001
+// 11110000N001
+// 111100000001
+// 111111111111
 /*============================================================================*/
 
 static int	ft_init_mlx(t_cub3d *data)
@@ -72,9 +68,9 @@ int	main(int argc, char **argv)
 	data.colors[3].g = 160;
 	data.colors[3].b = 221;
 
-	data.player.pos_x = 4; //* TEXTURE_SIZE + TEXTURE_SIZE / 2;
-	data.player.pos_y = 3; //* TEXTURE_SIZE + TEXTURE_SIZE / 2;
-	data.player.dir = 90;
+	data.player.pos_x = 9; //* TEXTURE_SIZE + TEXTURE_SIZE / 2;
+	data.player.pos_y = 7; //* TEXTURE_SIZE + TEXTURE_SIZE / 2;
+	data.player.dir = N;
 	data.player.fov = FOV * M_PI / 180;
 	data.player.plane_length = tan(data.player.fov / 2);
 /*========================================================================*/
