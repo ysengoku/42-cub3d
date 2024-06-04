1942d8e2e9f3c4bdbfdfb6262561223282e32314
Jason Mougel <jmougel@student.42lyon.fr>
2024-06-04 16:48:14 +0200

    merge from main

--- srcs/main.c
+++ srcs/main.c
@@ -6,62 +6,12 @@
 /*   By: jmougel <jmougel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 08:09:57 by yusengok          #+#    #+#             */
-/*   Updated: 2024/06/04 16:46:09 by jmougel          ###   ########.fr       */
+/*   Updated: 2024/06/04 16:47:10 by jmougel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-/* static int	ft_init_mlx(t_cub3d *data)
-{
-	data->mlx_ptr = mlx_init();
-	if (!(data->mlx_ptr))
-	{
-		ft_putendl_fd("MLX: Initialization failed", 2);
-		//== For test =============================
-		for (int i = 0; data->map.map[i]; i++)
-		free(data->map.map[i]);
-		free(data->map.map);
-		//=========================================
-		return (1);
-	}
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WIN_W, WIN_H, WINNAME);
-	if (!(data->win_ptr))
-	{
-		perror("MLX");
-		free(data->mlx_ptr);
-		//== For test =============================
-		for (int i = 0; data->map.map[i]; i++)
-		free(data->map.map[i]);
-		free(data->map.map);
-		//=========================================
-		return (1);
-	}
-
-	return (0);
-}
-
-static void	init_player(t_cub3d *data)
-{
-	if (data->map.player == 'N')
-		data->player.initial_dir = N;
-	else if (data->map.player == 'S')
-		data->player.initial_dir = S;
-	else if (data->map.player == 'W')
-		data->player.initial_dir = W;
-	else if (data->map.player == 'E')
-		data->player.initial_dir = E;
-	data->player.fov = FOV * M_PI / 180;
-	data->player.pos_x = 0; // will be set after parsing
-	data->player.pos_y = 0; // will be set after parsing
-	data->player.initial_dir = 0; // will be set after parsing
-	data->player.dir = 0.0; // will be set after parsing
-	data->player.dir_x = 0; // will be set in init_camera
-	data->player.dir_y = 0; // will be set in init_camera
-	data->player.plane_length = tan(data->player.fov / 2);
-	data->player.plane_x = 0; // will be set in init_camera
-	data->player.plane_y = 0; // will be set in init_camera
-	data->player.moved = 1; // must be always initialized to 1 to show initial view
 static void	init_player(t_player *player)
 {
 	player->fov = FOV * M_PI / 180;
@@ -82,10 +32,9 @@ static void init_cub3d_data(t_cub3d *data)
 	int	i;
 
 	i = -1;
-	//void		*mlx_ptr; ?
-	//void		*win_ptr; ?
-	ft_bzero(&data->img, sizeof(data->img));
-	ft_bzero(&data->map, sizeof(data->map));
+	data->mlx_ptr = 0;
+	data->win_ptr = 0;
+ 	ft_memset(&data->img, 0, sizeof(data->img));
 	init_player(&data->player);
 	data->ceiling_color = 0;
 	data->floor_color = 0;
@@ -162,46 +111,14 @@ int	main(int argc, char **argv)
 		return (1);
 	if (set_wall_texture(&data, data.wall) == 1)
 		return (1);
-	}
-	data.img.addr = mlx_get_data_addr(data.img.img, &data.img.bits_per_pxl,
-			&data.img.line_len, &data.img.endian);
-	if (set_texture(&data, data.wall) == 1)
+	if (create_minimap_img(&data, &data.mmap) == 1)
 		return (1);
-	if (BONUS)
-	{
-		data.mmap.img.img = mlx_new_image(data.mlx_ptr,
-				(data.map.map_len_x - 1) * MMAP_SCALE, data.map.map_len_y * MMAP_SCALE);
-		// NULL check
-		data.mmap.img.addr = mlx_get_data_addr(data.mmap.img.img,
-			&data.mmap.img.bits_per_pxl, &data.mmap.img.line_len,
-			&data.mmap.img.endian);
-	}
-	//=== TEST ===============================================================
-	if (BONUS)
-	{
-		data.mmap.floor.img = mlx_xpm_file_to_image(data.mlx_ptr, MMAP_F,
-				&data.mmap.floor.w, &data.mmap.floor.h);
-		data.mmap.floor.addr = mlx_get_data_addr(data.mmap.floor.img,
-				&data.mmap.floor.bits_per_pxl, &data.mmap.floor.line_len,
-				&data.mmap.floor.endian);
-		data.mmap.player.img = mlx_xpm_file_to_image(data.mlx_ptr, MMAP_PL,
-				&data.mmap.player.w, &data.mmap.player.h);
-		data.mmap.player.addr = mlx_get_data_addr(data.mmap.player.img,
-				&data.mmap.player.bits_per_pxl, &data.mmap.player.line_len,
-				&data.mmap.player.endian);
-		data.mmap.wall.img = mlx_xpm_file_to_image(data.mlx_ptr, MMAP_WL,
-				&data.mmap.wall.w, &data.mmap.wall.h);
-		data.mmap.wall.addr = mlx_get_data_addr(data.mmap.wall.img,
-				&data.mmap.wall.bits_per_pxl, &data.mmap.wall.line_len,
-				&data.mmap.wall.endian);
-	}
-	//========================================================================
 	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, handle_closebutton, &data);
 	mlx_hook(data.win_ptr, KeyPress, KeyPressMask, handle_keypress, &data);
 	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, handle_keyrelease, &data);
 	// mlx_mouse_hook(data.win_ptr, &handle_mouseevents, &data); // ---> Doesn't need ??
 	mlx_loop_hook(data.mlx_ptr, game_loop, &data);
-	mlx_loop(data.mlx_ptr); */
+	mlx_loop(data.mlx_ptr);
 	return (0);
 }
 
