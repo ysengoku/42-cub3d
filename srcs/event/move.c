9f9409bc413c19d25355b9e0aa757aed99451365
yusengok <ysengoku.tourisme@gmail.com>
2024-06-07 10:02:19 +0200

    Add another possible condition for wall collision (commented for the moment)

--- srcs/event/move.c
+++ srcs/event/move.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 09:59:51 by yusengok          #+#    #+#             */
-/*   Updated: 2024/06/05 16:24:05 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/06/07 09:52:26 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void	move_forward(t_cub3d *data, t_player *player, t_map *map)
 		player->pos_x += player->dir_x * MOVE;
 		player->pos_y += player->dir_y * MOVE;
 	}
+	// if (map->map[(int)player->pos_y][dest_x] != '1')
+	// 	player->pos_x += player->dir_x * MOVE;
+	// if (map->map[dest_y][(int)player->pos_x] != '1')
+	// 	player->pos_y += player->dir_y * MOVE;
 	data->player.moved = 1;
 }
 
@@ -43,6 +47,10 @@ void	move_backward(t_cub3d *data, t_player *player, t_map *map)
 		player->pos_x += player->dir_x * -1 * MOVE;
 		player->pos_y += player->dir_y * -1 * MOVE;
 	}
+	// if (map->map[(int)player->pos_y][dest_x] != '1')
+	// 	player->pos_x += player->dir_x * -1 * MOVE;
+	// if (map->map[dest_y][(int)player->pos_x] != '1')
+	// 	player->pos_y += player->dir_y * -1 * MOVE;
 	data->player.moved = 1;
 }
 
@@ -60,6 +68,10 @@ void	move_left(t_cub3d *data, t_player *player, t_map *map)
 		player->pos_x += player->dir_y * MOVE;
 		player->pos_y += player->dir_x * -1 * MOVE;
 	}
+	// if (map->map[(int)player->pos_y][dest_x] != '1')
+	// 	player->pos_x += player->dir_y * MOVE;
+	// if (map->map[dest_y][(int)player->pos_x] != '1')
+	// 	player->pos_y += player->dir_x * -1 * MOVE;
 	data->player.moved = 1;
 }
 
@@ -77,5 +89,9 @@ void	move_right(t_cub3d *data, t_player *player, t_map *map)
 		player->pos_x += player->dir_y * -1 * MOVE;
 		player->pos_y += player->dir_x * MOVE;
 	}
+	// if (map->map[(int)player->pos_y][dest_x] != '1')
+	// 	player->pos_x += player->dir_y * -1 * MOVE;
+	// if (map->map[dest_y][(int)player->pos_x] != '1')
+	// 	player->pos_y += player->dir_x * MOVE;
 	data->player.moved = 1;
 }
