12cb6637103a4d9f355b96549efff5f07461232f
yusengok <ysengoku.tourisme@gmail.com>
2024-06-10 08:32:03 +0200

    Fix player left & right move issue with north and south direction, fix reversed textures with some angles.

--- srcs/event/rotate.c
+++ srcs/event/rotate.c
@@ -6,13 +6,13 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 08:43:31 by yusengok          #+#    #+#             */
-/*   Updated: 2024/06/03 10:26:18 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/06/10 07:55:53 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	rotate_counterclockwise(t_cub3d *data)
+void	rotate_clockwise(t_cub3d *data)
 {
 	data->player.dir += ROTATE;
 	if (data->player.dir >= 360)
@@ -20,7 +20,7 @@ void	rotate_counterclockwise(t_cub3d *data)
 	data->player.moved = 1;
 }
 
-void	rotate_clockwise(t_cub3d *data)
+void	rotate_counterclockwise(t_cub3d *data)
 {
 	data->player.dir -= ROTATE;
 	if (data->player.dir < 0)
