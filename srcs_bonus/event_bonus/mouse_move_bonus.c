ce4af9b10fc513c8f8957af0c77021fe2a41bf9d
Jason Mougel <jmougel@z1r3p5.42lyon.fr>
2024-06-17 10:13:23 +0200

    :feat: add close door animation

--- srcs_bonus/event_bonus/mouse_move_bonus.c
+++ srcs_bonus/event_bonus/mouse_move_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mouse_move_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jmougel <jmougel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 12:35:23 by yusengok          #+#    #+#             */
-/*   Updated: 2024/06/17 08:52:34 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/06/17 10:07:17 by jmougel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,6 @@ int	mousemove(int x, int y, t_cub3d *data)
 		if (data->previous_mouse_x < x)
 			rotate_clockwise(data);
 	}
-	data->player.moved = 1;
 	data->previous_mouse_x = x;
 	return (0);
 }
-
-// int	mousescroll(int event, int x, int y, t_cub3d *data)
-// {
-// 	(void) x;
-// 	(void) y;
-// 	if (event == MOUSE_DOWN)
-// 		data->player.pitch += PITCH;
-// 	if (event == MOUSE_UP)
-// 		data->player.pitch -= PITCH;
-// 	data->player.moved = 1;
-// 	return (0);
-// }
