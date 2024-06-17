c10f923ca0b3339dba89b6838b59ca3ab3a424d2
yusengok <ysengoku.tourisme@gmail.com>
2024-06-17 10:13:00 +0200

    Remove mouse scroll fuction

--- srcs_bonus/event_bonus/mouse_move_bonus.c
+++ srcs_bonus/event_bonus/mouse_move_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 12:35:23 by yusengok          #+#    #+#             */
-/*   Updated: 2024/06/17 08:52:34 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/06/17 10:12:35 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,3 @@ int	mousemove(int x, int y, t_cub3d *data)
 	data->previous_mouse_x = x;
 	return (0);
 }
-
-// int	mousescroll(int event, int x, int y, t_cub3d *data)
-// {
-// 	(void) x;
-// 	(void) y;
-// 	if (event == MOUSE_DOWN)
-// 		data->player.pitch += PITCH;
-// 	if (event == MOUSE_UP)
-// 		data->player.pitch -= PITCH;
-// 	data->player.moved = 1;
-// 	return (0);
-// }
