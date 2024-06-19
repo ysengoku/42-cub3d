f4ae43e71ad8a37ba62477ed8fa8d88eab5b3f2a
yusengok <ysengoku.tourisme@gmail.com>
2024-06-19 13:43:50 +0200

    Update check hit & draw functions

--- srcs/rendering/raycasting.c
+++ srcs/rendering/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 10:25:16 by yusengok          #+#    #+#             */
-/*   Updated: 2024/06/19 10:26:22 by yusengok         ###   ########.fr       */
+/*   Updated: 2024/06/19 13:41:08 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,18 @@ void	raycasting(t_cub3d *data, int x, t_xpm_img *door)
 	set_ray(data, &ray, x);
 	draw_ceiling(data, x, data->win_half_h, data->ceiling_color);
 	draw_floor(data, x, data->win_half_h, data->floor_color);
-	check_wall_hit(data, &ray);
+	check_hit(data, &ray);
 	draw_wall(data, x, &ray);
 	if (BONUS)
 	{
-		reset_ray(data, &ray);
-		check_door_hit(data, &ray, x, 'D');
-		if (ray.hit == DOOR_CLOSE)
-			draw_door(data, x, &ray, &data->wall[DR_C]);
+		if (ray.close_door_hit)
+			draw_door(data, x, &ray, &ray.closed_d);
+		if (ray.open_door_hit)
+			draw_door(data, x, &ray, &ray.open_d);
 		reset_ray(data, &ray);
 		check_door_hit(data, &ray, x, 'A');
 		if (ray.hit == DOOR_ANIM)
-			draw_door(data, x, &ray, door);
-		reset_ray(data, &ray);
-		check_door_hit(data, &ray, x, 'O');
-		if (ray.hit == DOOR_OPEN)
-			draw_door(data, x, &ray, &data->wall[DR_O]);
+			draw_anim_door(data, x, &ray, door);
 		if (data->keys.key_pressed_x == 1)
 			draw_ray_mmap(data, &ray);
 	}
@@ -47,8 +43,8 @@ void	raycasting(t_cub3d *data, int x, t_xpm_img *door)
 
 static void	set_ray(t_cub3d *data, t_ray *ray, int x)
 {
-	ray->hit = NOTHING;
-	ray->w_dist = 0;
+	ray->closed_d.tex = DR_C;
+	ray->open_d.tex = DR_O;
 	ray->camera_p = 2 * x / (double)WIN_W - 1;
 	ray->dir.x = data->player.dir.x + data->player.plane.x * ray->camera_p;
 	ray->dir.y = data->player.dir.y + data->player.plane.y * ray->camera_p;
@@ -62,7 +58,7 @@ static void	set_ray(t_cub3d *data, t_ray *ray, int x)
 static void	reset_ray(t_cub3d *data, t_ray *ray)
 {
 	ray->hit = NOTHING;
-	ray->w_dist = 0;
+	ray->wall.dist = 0;
 	ray->map_x = (int)data->player.pos.x;
 	ray->map_y = (int)data->player.pos.y;
 	set_sidedist(ray, &data->player);
