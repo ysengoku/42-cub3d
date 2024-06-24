e7457807c3641d5ddd17bd98622d952c177947cf
yusengok <yusengok@student.42.fr>
2024-06-24 13:34:43 +0200

    Fix map check issue

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -3,47 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmougel <jmougel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yusengok <yusengok@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/01 16:54:57 by jmougel           #+#    #+#             */
-/*   Updated: 2024/06/24 11:24:32 by jmougel          ###   ########.fr       */
+/*   Updated: 2024/06/24 13:27:36 by yusengok         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-static void	display_map(char **map)
-{
-	int	i;
-
-	i = 0;
-	while (map[i])
-	{
-		printf("%s\n", map[i]);
-		i++;
-	}
-}
-
-static void	display_data(t_cub3d *data)
-{
-	printf("\n[DATA MAP]\n\n");
-	display_map(data->map.data_map);
-	printf("\n[DUP MAP]\n\n");
-	display_map(data->map.dup_map);
-	printf("\n[MAP]\n\n");
-	display_map(data->map.map);
-	printf("\n\n");
-	printf("[max len x] : %d\n", data->map.map_len_x);
-	printf("[max len y] : %d\n", data->map.map_len_y);
-	printf("[sprite_NO] : %s\n", data->wall[NO].path);
-	printf("[sprite_SO] : %s\n", data->wall[SO].path);
-	printf("[sprite_EA] : %s\n", data->wall[WE].path);
-	printf("[sprite_WE] : %s\n", data->wall[EA].path);
-	printf("[F_RGB]     : [R]%d, [G]%d, [B]%d\n",
-		data->map.f_rgb[0], data->map.f_rgb[1], data->map.f_rgb[2]);
-	printf("[C_RGB]     : [R]%d, [G]%d, [B]%d\n",
-		data->map.c_rgb[0], data->map.c_rgb[1], data->map.c_rgb[2]);
-}
 
 int	parsing(char *file, t_cub3d *data)
 {
@@ -62,7 +29,5 @@ int	parsing(char *file, t_cub3d *data)
 		return (EXIT_FAILURE);
 	if (check_map(data) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
-	display_map(data->map.dup_map);
-	display_data(data);
 	return (EXIT_SUCCESS);
 }
